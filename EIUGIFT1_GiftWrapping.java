727ffdc2e11f006dec2dd32e5a6e4e73d6084d57
thucvanminh <thucnichan2929vmt@gmail.com>
2025-01-10 18:38:59 +0700

    Quick

--- EIUGIFT1_GiftWrapping.java
+++ EIUGIFT1_GiftWrapping.java
@@ -2,7 +2,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.InputMismatchException;
 
 public class EIUGIFT1_GiftWrapping {
