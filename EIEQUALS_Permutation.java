2ca895b886d41e2dbbda696718185ef1ff98d456
thucvanminh <thucnichan2929vmt@gmail.com>
2025-01-22 13:49:30 +0700

    Quick

---
+++ EIEQUALS_Permutation.java
@@ -0,0 +1,176 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+
+public class EIEQUALS_Permutation {
+    public static void main(String[] args) throws IOException {
+        InputReader sc = new InputReader(System.in);
+        int n = sc.nextInt();
+        int change = sc.nextInt();
+        int differ = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (int i = 0; i < n; i++) {
+            int num = sc.nextInt();
+            if (map.get(num) != null) {
+                map.put(num, map.get(num) - 1);
+                if (map.get(num) == 0) {
+                    map.remove(num);
+                }
+            } else {
+                differ = num;
+            }
+        }
+        for (int i = 0; i < change; i++) {
+            differ++;
+            if (map.get(differ) != null) {
+                map.remove(differ);
+            }
+        }
+        for (int i = 0; i < change; i++) {
+            differ--;
+            if (map.get(differ) != null) {
+                map.remove(differ);
+            }
+        }
+
+        if (map.isEmpty()) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+
+    static class InputReader {
+        private byte[] inbuf = new byte[2 << 23];
+        public int lenbuf = 0, ptrbuf = 0;
+        public InputStream is;
+
+        public InputReader(InputStream stream) throws IOException {
+
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = System.in;
+            lenbuf = is.read(inbuf);
+        }
+
+        public InputReader(FileInputStream stream) throws IOException {
+            inbuf = new byte[2 << 23];
+            lenbuf = 0;
+            ptrbuf = 0;
+            is = stream;
+            lenbuf = is.read(inbuf);
+        }
+
+        public boolean hasNext() throws IOException {
+            if (skip() >= 0) {
+                ptrbuf--;
+                return true;
+            }
+            return false;
+        }
+
+        public String nextLine() throws IOException {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!isSpaceChar(b) && b != ' ') { // when nextLine, ()
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        public String next() {
+            int b = skip();
+            StringBuilder sb = new StringBuilder();
+            while (!(isSpaceChar(b))) { // when nextLine, (isSpaceChar(b) && b
+                                        // != ' ')
+                sb.appendCodePoint(b);
+                b = readByte();
+            }
+            return sb.toString();
+        }
+
+        private int readByte() {
+            if (lenbuf == -1)
+                throw new InputMismatchException();
+            if (ptrbuf >= lenbuf) {
+                ptrbuf = 0;
+                try {
+                    lenbuf = is.read(inbuf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (lenbuf <= 0)
+                    return -1;
+            }
+            return inbuf[ptrbuf++];
+        }
+
+        private boolean isSpaceChar(int c) {
+            return !(c >= 33 && c <= 126);
+        }
+
+        private double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        public Character nextChar() {
+            return skip() >= 0 ? (char) skip() : null;
+        }
+
+        private int skip() {
+            int b;
+            while ((b = readByte()) != -1 && isSpaceChar(b))
+                ;
+            return b;
+        }
+
+        public int nextInt() {
+            int num = 0, b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+
+        public long nextLong() {
+            long num = 0;
+            int b;
+            boolean minus = false;
+            while ((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'))
+                ;
+            if (b == '-') {
+                minus = true;
+                b = readByte();
+            }
+
+            while (true) {
+                if (b >= '0' && b <= '9') {
+                    num = num * 10 + (b - '0');
+                } else {
+                    return minus ? -num : num;
+                }
+                b = readByte();
+            }
+        }
+    }
+}
